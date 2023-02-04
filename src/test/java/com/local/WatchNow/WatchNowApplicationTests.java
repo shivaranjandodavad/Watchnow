f2a776208a52adff8c2d90bca6b2a18b448c68d1
Shivaranjan Dodavad <s>
2023-02-04 11:23:59 +0530

    new project

---
+++ src/test/java/com/local/WatchNow/WatchNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.WatchNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
